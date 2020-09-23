568048279befde1d69a00cdf062570ada67665cb
edin93 <il_matador@hotmail.fr>
2020-09-23 12:26:54 +0000

    task 0

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,7 @@
+function welcome(firstName, lastName) {
+    let fullName = firstName + ' ' + lastName;
+    function displayFullName() {
+	alert("Welcome " + fullName + "!");
+    }
+    displayFullName(fullName);
+}
