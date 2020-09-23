002cb0b8cbc822bbe3a1db99bac682212258bf18
edin93 <il_matador@hotmail.fr>
2020-09-23 12:42:28 +0000

    task 1 and 2

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,8 @@
+function welcomeMessage(fullName) {
+    return function() {
+	alert("Welcome " + fullName);
+    }
+}
+let guillaume = welcomeMessage("Guillaume");
+let alex = welcomeMessage("Alex");
+let fred = welcomeMessage("Fred");
