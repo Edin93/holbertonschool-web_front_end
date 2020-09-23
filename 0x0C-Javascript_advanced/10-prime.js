56a9c95c4de5c01f1f32a41bb70ee629145cd1f3
edin93 <il_matador@hotmail.fr>
2020-09-23 19:21:23 +0000

    task 10 done

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,22 @@
+function countPrimeNumbers() {
+    let n = 0;
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+        for (let j = 2; j <= parseInt(i / 2) + 1; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime) {
+            n += 1;
+        }
+    }
+    return n + 1;
+}
+
+let before = performance.now();
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+}
+let after = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${after - before} milliseconds.`)
