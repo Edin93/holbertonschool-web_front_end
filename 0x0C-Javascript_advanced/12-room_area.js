dfb0df5bdcddf9112a71a73c14a58cdaa007a44c
edin93 <il_matador@hotmail.fr>
2020-09-24 13:48:48 +0000

    task 12 done

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,10 @@
+let roomDimensions = {
+    'width': 50,
+    'length': 100,
+    'getArea': function() {
+        return (this.width * this.length);
+    }
+};
+
+let boundGetArea = roomDimensions.getArea();
+console.log(boundGetArea);
